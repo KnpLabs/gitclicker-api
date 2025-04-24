383145ce987fff4ca4921aeb78593162243175cc
ErwannRousseau <erwann.rousseau@icloud.com>
2025-04-24 15:20:14 +0200

    fix: express errors type

--- src/items/items.router.ts
+++ src/items/items.router.ts
@@ -6,7 +6,7 @@ import { Item, BaseItem } from './item.interface'
 export const itemsRouter = express.Router()
 
 // GET items
-itemsRouter.get('/', async (req: Request, res: Response) => {
+itemsRouter.get('/', async (_req: Request, res: Response) => {
   try {
     const items: Item[] = await ItemService.findAll()
 
@@ -18,14 +18,14 @@ itemsRouter.get('/', async (req: Request, res: Response) => {
 })
 
 // GET items/:id
-itemsRouter.get('/:id', async (req: Request, res: Response) => {
-  const id: number = parseInt(req.params.id, 10)
-
+itemsRouter.get('/:id', async (req: Request, res: Response, next) => {
+  const id = Number.parseInt(req.params.id, 10)
   try {
     const item: Item = await ItemService.find(id)
 
     if (item) {
-      return res.status(200).send(item)
+      res.status(200).send(item)
+      return next()
     }
 
     res.status(404).send('item not found')
@@ -36,7 +36,7 @@ itemsRouter.get('/:id', async (req: Request, res: Response) => {
 })
 
 // POST items
-itemsRouter.post('/', async (req: Request, res: Response) => {
+itemsRouter.post('/', async (req, res) => {
   try {
     const item: BaseItem = req.body
 
@@ -50,7 +50,7 @@ itemsRouter.post('/', async (req: Request, res: Response) => {
 })
 
 // PUT items/:id
-itemsRouter.put('/:id', async (req: Request, res: Response) => {
+itemsRouter.put('/:id', async (req: Request, res: Response, next) => {
   const id: number = parseInt(req.params.id, 10)
 
   try {
@@ -61,7 +61,8 @@ itemsRouter.put('/:id', async (req: Request, res: Response) => {
     if (existingItem) {
       const updatedItem = await ItemService.update(id, itemUpdate)
 
-      return res.status(200).json(updatedItem)
+      res.status(200).json(updatedItem)
+      return next()
     }
 
     const newItem = await ItemService.create(itemUpdate)
